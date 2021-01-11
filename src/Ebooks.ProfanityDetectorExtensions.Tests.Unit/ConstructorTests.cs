b36d5a3e473f1a7dd41f6b8803ef155d879df89a
Matthew Dunlop <wootismana@gmail.com>
2021-01-11 13:47:18 +0800

    Adding an extensions project that allows you to add a default list of profanities easily, without forcing everyone who wants the library to include the default json file

---
+++ src/Ebooks.ProfanityDetectorExtensions.Tests.Unit/ConstructorTests.cs
@@ -0,0 +1,22 @@
+﻿using Ebooks.ProfanityDetector;
+using System;
+using Xunit;
+
+namespace Ebooks.ProfanityDetectorExtensions.Tests.Unit
+{
+    public class ConstructorTests
+    {
+        [Fact]
+        public void Constructor_NoExceptionsThrown()
+        {
+            var filter = new ProfanityFilter().UseDefaults();
+        }
+
+        [Fact]
+        public void Constructor_ProfanitiesExist()
+        {
+            var filter = new ProfanityFilter().UseDefaults();
+            Assert.NotEmpty(filter.Terms.Prohibited);
+        }
+    }
+}
---
+++ src/Ebooks.ProfanityDetectorExtensions/ProfanityDetectorExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Reflection;
+
+namespace Ebooks.ProfanityDetector
+{
+    public static class ProfanityDetectorExtensions
+    {
+        public static ProfanityFilter UseDefaults(this ProfanityFilter filter)
+        {
+            // Read out the default filter object
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Ebooks.ProfanityDetectorExtensions.Resources.en_US.Terms.json";
+            string result;
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                result = reader.ReadToEnd();
+            }
+            
+            var terms = JsonConvert.DeserializeObject<Terms>(result);
+
+            // Append it to the terms already in the ProfanityFilter
+            filter.Terms.Prohibited.UnionWith(terms.Prohibited);
+            filter.Terms.Permitted.UnionWith(terms.Permitted);
+
+            // Return the instance back to allow for chaining
+            return filter;
+        }
+    }
+}
